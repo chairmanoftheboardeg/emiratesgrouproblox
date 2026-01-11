8ba2e5f5476984ceda4754a8d5a060bbf849b761
Tyler Nicholas <chairman@emiratesgrouproblox.link>
2026-01-11 10:47:40 +0400

    Delete assets/js directory

--- assets/js/supabase-config.js
+++
@@ -1,5 +0,0 @@
-// /assets/js/supabase-config.js
-// Paste from Supabase Project Settings -> API
-window.SUPABASE_URL = "https://dvcbbvvxbpxzpxaadoco.supabase.co";
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2Y2JidnZ4YnB4enB4YWFkb2NvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgwNjQ4NTAsImV4cCI6MjA4MzY0MDg1MH0.gzHjXYOts3Af2iXRPRT4KMnS7geimB0Z6BQyqojtu3w";
-
