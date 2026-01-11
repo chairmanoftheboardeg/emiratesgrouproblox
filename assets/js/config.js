2851d984509e5e7ccb4f5d00742889b9f348be9c
Tyler Nicholas <chairman@emiratesgrouproblox.link>
2026-01-11 10:59:55 +0400

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,18 +1,19 @@
 // /assets/js/config.js
 // Replace these with your real values.
 window.EGR_CONFIG = {
-  supabaseUrl: "PASTE_YOUR_SUPABASE_URL",
-  supabaseAnonKey: "PASTE_YOUR_SUPABASE_ANON_KEY",
+  supabaseUrl: "https://dvcbbvvxbpxzpxaadoco.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2Y2JidnZ4YnB4enB4YWFkb2NvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgwNjQ4NTAsImV4cCI6MjA4MzY0MDg1MH0.gzHjXYOts3Af2iXRPRT4KMnS7geimB0Z6BQyqojtu3w
+",
 
   // Discord widget (requires enabling "Server Widget" in Discord server settings)
-  discordGuildId: "YOUR_DISCORD_GUILD_ID",
-  discordInviteUrl: "https://discord.gg/YOUR_INVITE_CODE",
+  discordGuildId: "1189224375019388999",
+  discordInviteUrl: "https://discord.gg/nDAZ3p475V",
 
   // Redirect targets
-  careersUrl: "https://discord.gg/YOUR_INVITE_CODE",
-  contactUsUrl: "https://discord.com/channels/YOUR_SERVER_ID/YOUR_CONTACT_CHANNEL_ID",
-  reportUrl: "https://discord.com/channels/YOUR_SERVER_ID/YOUR_REPORT_CHANNEL_ID",
-  flydubaiDiscordUrl: "https://discord.gg/YOUR_FLYDUBAI_INVITE",
+  careersUrl: "https://careers.emiratesgrouproblox.link",
+  contactUsUrl: "https://discord.com/channels/1189224375019388999/1454752184683200615",
+  reportUrl: "https://discord.com/channels/1189224375019388999/1454752184683200615",
+  flydubaiDiscordUrl: "https://discord.gg/nDAZ3p475V",
 
   // Optional external flights board (if you have one)
   flightsUrl: "https://flights.emiratesgrouproblox.link/"
